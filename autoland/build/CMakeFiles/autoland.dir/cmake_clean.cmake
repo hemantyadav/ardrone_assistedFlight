c5e01e4affab02db2be6fc67c615a0b7309e6204
fare0015 <fare0015@umn.edu>
2013-04-18 18:07:25 -0500

    Upload autoland module

---
+++ autoland/build/CMakeFiles/autoland.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/autoland.dir/src/main.o"
+  "../bin/autoland.pdb"
+  "../bin/autoland"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/autoland.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
